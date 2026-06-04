68e14d76a10611f22b72f4accbd61be6a427334c
Khoi <khoibui0224@gmail.com>
2026-06-03 21:02:55 -0400

    Updated Code

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,31 +13,55 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+  const isbn = req.params.isbn;
+  if (isbn >= 1 && isbn <= 10) {
+    return res.send(books[isbn]);
+  } else {
+    return res.status(400).json({ message: "Invalid ISBN number " });
+  }
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  for (const book of Object.values(books)) {
+    let processed = book.author.toLowerCase().replace(/\s+/g, '');
+    if (processed === author) {
+        return res.send(book);
+    }
+  }
+  return res.status(400).json({ message: "Unable to find author "});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  for (const book of Object.values(books)) {
+    let processed = book.title.toLowerCase().replace(/\s+/g, '');
+    if (processed === title) {
+        return res.send(book);
+    }
+  }
+  return res.status(400).json({message: "Unable to find title "});
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  if (isbn >= 1 && isbn <= 10) {
+    return res.send(books[isbn].reviews);
+  } else {
+    return res.status(400).json({ message: "Invalid ISBN number "});
+  }
 });
 
 module.exports.general = public_users;
