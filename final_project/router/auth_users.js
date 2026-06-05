06176b906c2daf33005d761820b23cb536207bd2
Khoi <khoibui0224@gmail.com>
2026-06-04 21:13:10 -0400

    Finished task 8 and 9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -86,6 +86,27 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   });
 });
 
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const username = req.session.authorization?.username;
+
+  if (!username) {
+    return res.status(400).json({ message: "User not logged in" });
+  }
+
+  if (!books[isbn]) {
+    return res.status(401).json({ message: "Book not found" });
+  }
+
+  if (!books[isbn].reviews[username]) {
+    return res.status(401).json({ message: "Review of book not found" });
+  }
+  
+  delete books[isbn].reviews[username];
+  return res.status(200).json({ message: `Review of the book ${books[isbn].title} deleted.` });
+});
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -22,10 +22,13 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+/*public_users.get('/',function (req, res) {
   //Write your code here
   return res.send(JSON.stringify(books,null,4));
-});
+});*/
+
+// Get the book list available in the shop using promises
+
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
